332c280b08e4f02df033c139fdb94a12f8350bdb
Ivo Fernandes <ivoadf@gmail.com>
2016-04-22 22:24:51 +0100

    test network

--- Interface/js/ctr_bundle.js
+++ Interface/js/ctr_bundle.js
@@ -26,6 +26,33 @@ window.trainNetwork = function (node_attr, train_set, options){
   return net.train(train_array,options)
 }
 
+window.testNetwork = function(node_attr, test_set){
+  var test_array = [];
+  var outputs = [];
+  for(i=0;i<test_set.length;i++){
+    input_hash = {};
+    output_hash = {};
+    Object.keys(test_set[i]).forEach(function (key) {
+      var value = test_set[i][key];
+      if(node_attr[key]=='0'){//input node
+        input_hash[key] = value;
+      }
+      else if(node_attr[key]=='1'){//output node
+        output_hash[key] = value;
+      }
+    })
+    test_array.push(input_hash);
+    var out_hash = {};
+    out_hash['expected'] = output_hash;
+    outputs.push(out_hash);
+  }
+
+  for(i=0;i<test_array.length;i++){
+    outputs[i]['net_result']=net.run(test_array[i]);
+  }
+  return outputs;
+}
+
 },{"brain":2}],2:[function(require,module,exports){
 exports.NeuralNetwork = require("./neuralnetwork").NeuralNetwork;
 exports.crossValidate = require("./cross-validate");
